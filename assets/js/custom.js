d8d63b5c33cf041df9e27ac287b382d1bbb22910
Naveen-Ninu <naveenninu888@gmail.com>
2022-09-23 16:59:12 +0530

    new-page

---
+++ assets/js/custom.js
@@ -0,0 +1,39 @@
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// preloader
+
+var preloader = document.getElementById("loading");
+var body = document.querySelector("body");
+var html = document.querySelector("html");
+setTimeout(function loader() {
+  console.log("preloader");
+  preloader.style.display = "none";
+  body.classList.remove("no-scroll-y");
+  // body.classList.add('no-scroll-x');
+  html.classList.add("no-scroll-x");
+}, 3000);
+
+// Aos_Animation-----------------------
+AOS.init({
+  once: true,
+});
